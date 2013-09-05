d89bf4c6cc8a8c9f454af9d52c4625c0afc66faa
diegoliv <diego@favolla.com.br>
2013-09-05 20:07:45 -0300

    added dashicons, basic layout for look like a wp admin area, and more changes on the main code

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ $(function(){
   post.Block = Backbone.Model.extend({
     defaults: {
       tag: 'div',
+      remove: true,
+      move: true,
       content: 'Type here'
     }
   });
@@ -26,11 +28,7 @@ $(function(){
     model: post.Block
   })
 
-  post.blocks = new post.Blocks({
-    id:1,
-    tag: 'div',
-    content: 'Type your content here...'
-  });
+  post.blocks = new post.Blocks();
 
   // the blocks view
   post.BlockView = Backbone.View.extend({
@@ -39,11 +37,11 @@ $(function(){
     template: _.template($('#post-block').html()),
 
     events: {
-      'click .remove': 'destroy'
+      'click span.remove': 'destroy'
     },
 
     initialize: function(){
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', 'unrender');
       this.model.bind('destroy', this.unrender);
       this.render();
     },
@@ -51,15 +49,17 @@ $(function(){
       this.$el.html(this.template(
         {
           block_tag: this.model.get('tag'),
-          block_content: this.model.get('content')
+          block_content: this.model.get('content'),
+          remove: this.model.get('remove'),
+          move: this.model.get('move')
         }
       ));
       
       return this;
     },
     unrender: function(){
-      console.log('unrender');
-      $(this.el).remove();
+      // console.log('unrender');
+      this.$el.remove();
     },
     destroy: function(){
       // console.log('destroy');
@@ -81,17 +81,28 @@ $(function(){
       _.bindAll(this, 'render', 'addBlock', 'updateTitle');
       this.wrapper = this.$el.find('#content');
       this.render();
+
+      // console.log(post.blocks);
     },
     render: function(){
       // initialize the post model
       post.thePost = new post.Data();
-      this.wrapper.html(this.template({post_title: post.thePost.get('title')}));
+      this.wrapper.append(this.template({post_title: post.thePost.get('title')}));
+
+      // insert first content block
+      var initBlock = new post.Block();
+      initBlock.set({remove: false});
+      post.blocks.add(initBlock);
+
+      var initView = new post.BlockView({model:initBlock});
+      this.wrapper.append(initView.render().el);
     },
-    addBlock: function(){
-      console.log('add block menu');
+    addBlock: function(e){
+
+      e.preventDefault();
 
       var block = new post.Block();
-      post.blocks.add(block)
+      post.blocks.add(block);
 
       var newBlock = new post.BlockView({model:block});
       this.wrapper.append(newBlock.render().el);
@@ -104,153 +115,5 @@ $(function(){
   });
 
   post.view = new post.View();
-
-  // // var answer = new app.Answer();
-
-  // app.AnswerList = Backbone.Collection.extend({
-  //   model: app.Answer,
-  //   localStorage: new Store("backbone-answers"),
-  // });
-
-  // // instance of the Collection
-  // app.answerList = new app.AnswerList();
-
-  // // app.answerList.on('add change', function(){
-  // //   console.log(JSON.stringify(app.answerList));
-  // // });
-
-  // app.AnswerView = Backbone.View.extend({
-  //   tagName: 'tr',
-  //   className: 'answer',
-  //   template: _.template($('#answer-template').html()),
-
-  //   events: {
-  //     'click button.destroy': 'destroy',
-  //     'change input.answer-edit' : 'updateLabel',
-  //     'click button.edit' : 'editLabel',
-  //     'click button.save' : 'saveLabel',
-  //     'change input.toggle': 'toggleAnswer',      
-  //   },
-
-  //   initialize: function(){
-  //     _.bindAll(this, 'render','remove', 'unrender', 'updateLabel', 'editLabel', 'saveLabel','toggleAnswer');
-  //     this.model.bind('change', this.render);
-  //     this.model.bind('destroy', this.unrender);
-  //     this.render();
-  //   },
-  //   render: function(){
-  //     $(this.el).html(this.template(
-  //       {
-  //         title: this.model.get('title'), 
-  //         isAnswer: this.model.get('isAnswer'), 
-  //         number: this.model.get('number')
-  //       }
-  //     ));
-
-  //     $(this.el).find('.answer-edit').hide();
-  //     $(this.el).find('.save').hide();
-
-  //     return this;
-  //   },
-  //   unrender: function(){
-  //     // console.log('unrender');
-  //     $(this.el).remove();
-  //   },
-  //   destroy: function(){
-  //     // console.log('destroy');
-  //     this.model.destroy();
-  //   },
-  //   editLabel: function(e){
-  //     $(e.target).hide();
-  //     $(this.el).find('.save').show();
-  //     $(this.el).find('.answer-text').hide();
-  //     $(this.el).find('.answer-edit').show();
-  //   },
-  //   updateLabel: function(e){
-  //     var newText = $(e.target).val();
-  //     this.model.set({title: newText});
-  //   },
-  //   saveLabel: function(e){
-  //     $(e.target).hide();
-  //     $(this.el).find('.edit').show();
-  //     $(this.el).find('.answer-edit').hide();
-  //     $(this.el).find('.answer-text').show();
-  //   },
-  //   toggleAnswer: function(e){
-  //     var all = app.answerList.where({isAnswer:true});
-
-  //     _.each(all, function(item){
-  //       item.set({isAnswer:false});
-  //     });
-
-  //     if($(e.target).val() == 'on'){
-  //       // console.log($(e.target).val());
-  //       this.model.set({ isAnswer: true });
-  //     } else {
-  //       this.model.set({ isAnswer: false });
-  //     }
-  //   }
-  // });
-
-
-  // app.AppView = Backbone.View.extend({
-  //   el: $('#body'),
-
-  //   events: {
-  //     'click button#insert-button': 'addAnswer',
-  //     'click button#get-answer': 'getAnswer',
-  //   },
-
-  //   initialize: function(){
-  //     _.bindAll(this, 'addAnswer', 'getAnswer');
-
-  //     this.collection = new app.AnswerList();
-  //     this.counter = 0;
-  //   },
-  //   addAnswer: function(){
-  //     this.counter++;
-  //     var answer = new app.Answer();
-  //     answer.set({
-  //       number: this.counter
-  //     });
-
-  //     app.answerList.add(answer);
-
-  //     var newAnswer = new app.AnswerView({model: answer});
-
-  //     $('tbody',this.el).append(newAnswer.render().el);
-  //   },
-  //   getAnswer: function(){
-  //     var theAnswer = app.answerList.where({isAnswer: true});
-  //     var answr = {};
-  //     _.each(theAnswer, function(item){
-  //       answr = item.toJSON();
-  //     });
-  //     alert(answr.title);
-  //   }
-
-  // });
-
-  // app.appView = new app.AppView();
-
-
-  // // view para dar output na collection
-  // app.Output = Backbone.View.extend({
-  //   el: '#output',
-
-  //   initialize: function(){
-  //     // this.model.bind('collectionChange', this.render);
-  //     _.bindAll(this,'render');
-  //     app.answerList.bind('add change destroy', this.render);
-  //     this.render();
-  //   },
-  //   render: function(){
-  //     var string = JSON.stringify(app.answerList, null, 2);
-  //     this.$el.html('<pre><code>'+ string + '</code></pre>');
-  //   }
-
-  // });
-
-  // app.output = new app.Output();
 
 });
