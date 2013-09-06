be5facc36b915293a6cee65201e594b61269344a
diegoliv <diego@favolla.com.br>
2013-09-06 20:36:43 -0300

    added sortable jQuery UI plugin and basic functionality to sort blocks

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ wp-contentblocks-prototype
 ==========================
 
 My personal prototyping repo for help developing Content Blocks feature for Wordpress-UI group.
+
+###Updates so far:
+
+- 09/06/2013: added Sortable jQuery UI plugin, with basic functionality to sort blocks;
+- 09/05/2013: added initial files;
+
+###Todo
+
+- Create a collection of Block models, each model is a different Block type, and their respective view's templates;
+- Improve sortable functions to handle side-by-side content blocks, columns, etc;
+- Develop the UI for choosing Which Content Block the user want to insert;
+- implement editor capability for the Blocks (suggestion: etch.js plugin);
+- UI improvements (handles, transitions, etc);
--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ $(function(){
   // Block default model
   post.Block = Backbone.Model.extend({
     defaults: {
+      wp_id: 0,
       tag: 'div',
       remove: true,
       move: true,
@@ -37,7 +38,8 @@ $(function(){
     template: _.template($('#post-block').html()),
 
     events: {
-      'click span.remove': 'destroy'
+      'click span.remove': 'destroy',
+      'mousedown .wp-block': 'addFocus',
     },
 
     initialize: function(){
@@ -48,13 +50,13 @@ $(function(){
     render: function(){
       this.$el.html(this.template(
         {
+          wp_id: this.model.get('wp_id'),
           block_tag: this.model.get('tag'),
           block_content: this.model.get('content'),
           remove: this.model.get('remove'),
           move: this.model.get('move')
         }
-      ));
-      
+      ));      
       return this;
     },
     unrender: function(){
@@ -65,6 +67,9 @@ $(function(){
       // console.log('destroy');
       this.model.destroy();
     },
+    addFocus: function(e){
+      $(e.target).focus();
+    }
   });
 
   // the post view
@@ -79,33 +84,60 @@ $(function(){
 
     initialize: function(){
       _.bindAll(this, 'render', 'addBlock', 'updateTitle');
-      this.wrapper = this.$el.find('#content');
+      this.container = $('#content');
+      this.wrapper = $('#content-blocks');
+      
+      // counter for the views
+      this.counter = 0;
+
       this.render();
 
-      // console.log(post.blocks);
+      // initial state of sortable plugin
+      this.wrapper.sortable({
+        handle: '.move',
+        containment: '#content',
+        connectWith: ".content-blocks",
+      }).disableSelection();
+
     },
     render: function(){
       // initialize the post model
       post.thePost = new post.Data();
-      this.wrapper.append(this.template({post_title: post.thePost.get('title')}));
+      this.container.prepend(this.template({post_title: post.thePost.get('title')}));
 
       // insert first content block
+      this.counter++;
       var initBlock = new post.Block();
-      initBlock.set({remove: false});
+      initBlock.set({
+        wp_id: this.counter,
+        remove: false
+      });
+      
+      // add to collection
       post.blocks.add(initBlock);
 
+      // bind initial model to a new instance of BlockView
       var initView = new post.BlockView({model:initBlock});
       this.wrapper.append(initView.render().el);
     },
     addBlock: function(e){
 
+      // prevent default behavior
       e.preventDefault();
-
-      var block = new post.Block();
+      
+      // creates a new instance of Block model
+      this.counter++;
+      var block = new post.Block({
+        wp_id: this.counter
+      });
+      
+      // add to collection
       post.blocks.add(block);
-
+      
+      // creates a new instance of BlockView and binds the new model to it
       var newBlock = new post.BlockView({model:block});
       this.wrapper.append(newBlock.render().el);
+
     },
     updateTitle:function(e){
       post.thePost.set({title: $(e.target).val()});
